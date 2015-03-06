7be4cffa081836cf0dad552b2a3c34c5920ee66c
Vasiliy Alferov <ya-ikmik2012@yandex.ru>
2015-03-06 15:58:41 +0000

    smth

---
+++ W2/helpers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This is a library that provides access to helpers modules to W2 directory
+
+                  It is a part of mammoth-world project
+                (https://github.com/vasalf/mammoth-world)
+
+"""
+
+import sys
+sys.path += "./helpers"
+
+import polygons as polygons
+import colored as colored
+
+__author__ = "vasalf"
